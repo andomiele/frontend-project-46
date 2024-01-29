8ffcced6f789a73d6199335fbf3115df2c11814f
Andomiele <alina2005@ya.ru>
2024-01-29 20:58:31 +0300

    all

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import * as fs from 'node:fs';
-;
+import process from 'node:process';
+import cwd from 'node:process';
+
 
 const readFile = (file1, file2) => {
   const path1 = path.resolve(process.cwd(__fixtures__/file1.json), '/__fixtures__/file1.json')
-  const data1 = fs.readFileSync(path1);
-  console.log(data1.toString());
+  const data1  = fs.readFileSync(path1);
+  console.log(data1 .toString());
 
   const path2 = path.resolve(process.cwd(__fixtures__/file2.json), '/__fixtures__/file1.json')
-  const data2 = fs.readFileSync(path2);
-    console.log(data2.toString());
+  const data2  = fs.readFileSync(path2);
+    console.log(data2 .toString());
 };
 export default readFile;
