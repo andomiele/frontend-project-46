bccce136f50209e2d477457fa9b18a45a54631ae
Andomiele <alina2005@ya.ru>
2024-02-01 18:06:19 +0300

    add getdiff in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import { readFileSync } from 'fs';
 import parse from '../src/parsers.js'
-import sortBy from 'lodash'
+import _ from 'lodash'
 
 const bealdFullPass = (filePath) => path.resolve(process.cwd(), filePath);
 
@@ -13,8 +13,27 @@ const readFile = (fileName) => {
 const genDiff = (filepath1, filepath2) => {
   const data1 = readFile(filepath1);
   const data2 = readFile(filepath2);
-  const sortData1 = sortBy(data1)
-  return sortData1;
-
+  // const entries1 = Object.entries(data1);
+  // const entries2 = Object.entries(data2);
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const sortKeys = _.sortBy(_.union(keys1, keys2));
+  let result = '';
+  const res = {};
+  for (const key of sortKeys) {
+    if ((data1[key] !== undefined && data2[key] === undefined)) {
+      result += (`  - ${res[key] = key}: ${data1[key]}\n`);
+    }
+    else if ((data1[key] === undefined && data2[key] !== undefined)) {
+      result += (`  + ${res[key] = key}: ${data2[key]}\n`);
+    }
+    else if ((data1[key] !== undefined && data2[key] !== undefined) && (data1[key] ===  data2[key])) {
+      result += (`    ${res[key] = key}: ${data1[key]}\n`);
+    }
+    else if ((data1[key] !== undefined && data2[key] !== undefined) && (data1[key] !== data2[key])) {
+      result += (`  - ${res[key] = key}: ${data1[key]}\n  + ${res[key] = key}: ${data2[key]}\n`);
+    }
+  }
+  return (`{\n${result}}`);
 };
 export default genDiff;
